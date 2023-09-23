15ea884bc3c3e69dab1a2d1946e0413e24e723f2
Gabriel Souza <gabrielmacielsouza537@gmail.com>
2023-09-23 17:52:09 -0300

    initial commit :tada:

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align="center"> PickAFlick </h1>
+
+<h6 align="center"> 
+	If you want to preview the application, click <a href="#">here</a>.
+</h6>
+
+## 💻 Project
+
+> This project is a simple web page to display a random movie every time you click on Find Movie.
+
+The idea is:
+
+PickAFlick is a simple web page to display a random movie every time you click on Find Movie.
+
+Made with ♥ by Gabriel :wave:
+
+## 🚀 Technologies
+
+- HTML
+- CSS
+- JavaScript
---
+++ assets/javascript/script.js
@@ -0,0 +1,44 @@
+function searchMovies() {
+  $(".container").empty();
+
+  function gerarNumeroInteiroAleatorio(min, max) {
+    // Use Math.floor para arredondar o número para baixo e Math.random()
+    // para gerar um número aleatório entre [min, max].
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const numeroAleatorio = gerarNumeroInteiroAleatorio(1, 100);
+
+  $.ajax({
+    url:
+      "https://api.themoviedb.org/3/movie/" +
+      numeroAleatorio +
+      "?api_key=9ab6c07ab0aab370a9aea47a98dbed7a&language=pt-BR",
+    method: "GET",
+    success: function (data) {
+      if (!data.overview || !data.original_title || !data.poster_path) {
+        searchMovies();
+      } else {
+        $(".container").append(`<div class="movie-info">
+        <img src="https://image.tmdb.org/t/p/w500${data.poster_path}" width="171" height="243.77" alt="" class="movie-poster" />
+      </div>
+      <div>
+        <h2 class="movie-title">
+          ${data.original_title}
+        </h2>
+        <p class="movie-description">
+          ${data.overview}
+        </p>
+      </div>`);
+      }
+    },
+    error: function (xhr, status, error) {
+      console.error("Ocorreu um erro: " + error);
+      searchMovies();
+    },
+  });
+}
+
+$("#describe-movies").click(function () {
+  searchMovies();
+});
